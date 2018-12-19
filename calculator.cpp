ea0144320e8c15c637179b05c2b9fc92f71093db
jim890717 <43335027+jim890717@users.noreply.github.com>
2018-12-19 17:14:52 +0800

    1.

---
+++ calculator.cpp
@@ -0,0 +1,67 @@
+#include "stafx.h"
+/*計算*/
+
+void cal()
+{
+
+    int a,b,c;
+    char ch;
+
+
+    scanf("%d",&a);
+    while(1)
+    {
+        if(scanf("%d",&b)==EOF)
+        {
+            break;
+        }
+        else
+        {
+
+            printf("Choice (+ - * /):");
+            scanf("%c",&ch);
+
+            ch=getche();
+            if(ch=='+')
+            {
+                printf("\n");
+                c=a+b;
+                printf("%d\n",c);
+            }
+            else if(ch=='-')
+            {
+                printf("\n");
+                c=a-b;
+                printf("%d\n",c);
+            }
+            else if(ch=='*')
+            {
+                printf("\n");
+                c=a*b;
+                printf("%d\n",c);
+            }
+            else if(ch=='/')
+            {
+                printf("\n");
+                c=a/b;
+                printf("%d\n",c);
+            }
+            a=c;
+
+
+
+        }
+
+
+
+
+
+
+    }
+
+
+
+
+}
+
+
