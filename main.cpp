c3eab9e6be27612244c501f440869559c844f09a
jim890717 <43335027+jim890717@users.noreply.github.com>
2018-12-19 17:09:52 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include "stafx.h"
-
+#include "user.h"
 
 int main()
 {
@@ -13,19 +13,27 @@ int main()
     scanf("%d",&choice);
 
     while(choice!=4){
-        
+
         switch(choice){
         case 1:
+            cal();
+
             break;
         case 2:
             break;
         case 3:
             break;
-            
-            
-            
+
+
+
         }
-        
+    printf("1.Simple calculation:\n");
+    printf("2.Custom formula:\n");
+    printf("3.Custom parameters:\n");
+    printf("4.Exit\n");
+    printf("=> ");
+    scanf("%d",&choice);
+
 
 
     }
