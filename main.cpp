f558dd9671bbc785a524e45ad3cc311dcf84ea52
jim890717 <43335027+jim890717@users.noreply.github.com>
2018-12-19 16:32:09 +0800

    main

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include "stafx.h"
+
+
+int main()
+{
+
+    int choice;
+    printf("1.Simple calculation:\n");
+    printf("2.Custom formula:\n");
+    printf("3.Custom parameters:\n");
+    printf("4.Exit\n");
+    printf("=> ");
+    scanf("%d",&choice);
+
+    while(choice!=4){
+        
+        switch(choice){
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+            
+            
+            
+        }
+        
+
+
+    }
+
+
+}
