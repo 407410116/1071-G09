ce0cf6f5c20e7a8b1a8bba02d035642e556ff43d
407410116 <43335270+407410116@users.noreply.github.com>
2018-12-19 23:06:48 +0800

    Update user.h

--- user.h
+++ user.h
@@ -4,4 +4,21 @@
 //原型宣告
 void cal();
 
+/*struct Support
+{
+    char *supfunc;
+};
+
+typedef struct ListNode
+{
+    Support element;
+    char *inputfunc;
+    ListNode *nextPtr;
+}node;
+
+typedef node *nodePtr;
+
+void instructions(void);
+void stoFun(nodePtr *sptr, node item);*/
+
 #endif // USER_H_INCLUDED
