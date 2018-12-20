c4d4ed88e0802c66fa808d71fa395758db1abe98
jim890717 <43335027+jim890717@users.noreply.github.com>
2018-12-20 11:31:23 +0800

    表單

---
+++ instruction.cpp
@@ -0,0 +1,12 @@
+#include "stafx.h"
+
+
+void instruction(){
+
+    printf("1.Simple calculation:\n");
+    printf("2.Custom formula:\n");
+    printf("3.Custom parameters:\n");
+    printf("4.Exit\n");
+    printf("=> ");
+}
+
