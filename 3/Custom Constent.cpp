2107b04014d2a2aca8dad46db329ec5bddfafe14
梁育修 <43335328+willy5423@users.noreply.github.com>
2018-12-21 18:54:52 +0800

    Update Custom Constent.cpp

--- 3/Custom Constent.cpp
+++ 3/Custom Constent.cpp
@@ -3,37 +3,49 @@
 #include <string.h>
 
 struct Constent
-    {
-    char *Letter[];
+{
+    char Letter[];
     double num[];
-    };
+};
 
-int check(char *N, int i)
+Constent constent;
+
+int check(char N, int i)
+{
+    for(int n=0; n<i; n++)
     {
-    for(int n=0;n<i;n++)
+        if(constent.Letter[n] == N)
         {
-        if(strcamp(constent.Letter[n],N) == 0)
-            {
-            printf("Already have this Constent");
-            break;
-            }
+            printf("Already have this Constent\n\n");
+            return 0;
         }
     }
+}
+
+int search(char N, int i)
+{
+    for(int n=1; n<=i; n++)
+    {
+        if(constent.Letter[n] == N)
+            printf("---%lf\n",constent.num[n]);
+        else
+            printf("Can't found this Constent.\n");
+    }
+}
+
 
 int main()
 {
     int choose;
     int i=0;
-    Constent constent;
 
-    Letter[] = (char*) malloc(sizeof(char)*20);
-
-    while(choose != 3)
+    while(choose != 4)
     {
         printf("Choose the next move :\n"
                "1. Insert a Constent.\n"
                "2. Delete a Constent.\n"
-               "3. Back.\n"
+               "3. Search a Constent.\n"
+               "4. Back.\n"
                "-->");
         scanf("%d", &choose);
         printf("\n");
@@ -41,20 +53,32 @@ int main()
         switch(choose)
         {
         case 1:
-                i++;
+            i++;
             printf("Enter a Letter : ");
             fflush(stdin);
-            scanf("%s", &constent.Letter[i]);
-                check(constent.Letter[i],i);
-            printf("Enter a Constent : ");
-            scanf("%lf", &constent.num[i]);
-            printf("%s = %lf\n", constent.Letter[i], constent.num[i]);
-            
+            scanf("%c", &constent.Letter[i]);
+            if(check(constent.Letter[i],i) == 0)
+                break;
+            else
+            {
+                printf("Enter a Constent : ");
+                scanf("%lf", &constent.num[i]);
+                printf("%c = %lf\n", constent.Letter[i], constent.num[i]);
+
+                printf("\n\n");
+            }
+            break;
+        case 2:
+
             printf("\n\n");
             break;
-        case 2:                               //還沒做
-        	
-        	printf("\n\n");
+        case 3:
+        	printf("Enter a Letter : ");
+        	fflush(stdin);
+            scanf("%c", &constent.Letter[i]);
+            search(constent.Letter[i],i);
+            
+            printf("\n\n");
             break;
         }
     }
