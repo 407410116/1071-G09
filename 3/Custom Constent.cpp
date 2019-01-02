aeea53b2b2b2c08db10ad2408f47bb98d74ab0f2
梁育修 <43335328+willy5423@users.noreply.github.com>
2019-01-02 17:32:47 +0800

    Update Custom Constent.cpp

--- 3/Custom Constent.cpp
+++ 3/Custom Constent.cpp
@@ -2,41 +2,57 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define MAX 1000
+
 struct Constant
 {
-    char Letter[];
-    double num[];
+    char Letter[MAX];
+    double num[MAX];
 };
 
 Constant constant;
 
-int check(char N, int i)
+int check_1(char N, int i)
 {
-    for(int n=0; n<i; n++)
+    for(int n=1; n<i; n++)
     {
         if(constant.Letter[n] == N)
-        {
-            printf("Already have this Constant\n\n");
             return 0;
-        }
     }
 }
 
-int search(char N, int i)           //有問題
+int check_2(char N, int i)
 {
-    for(int n=0; n<i; n++)
+    for(int n=1; n<=i; n++)
     {
         if(constant.Letter[n] == N)
-            printf("---%lf\n",constant.num[n]);
-        else
-            printf("Can't found this Constant.\n");
+            return 0;
     }
 }
 
+int search(char N, int i)
+{
+    for(int n=1; n<=i; n++)
+    {
+        if(constant.Letter[n] == N)
+            return n;
+    }
+}
+
+int del(char N, int i)
+{
+    for(int n=1; n<=i; n++)
+    {
+        if(constant.Letter[n] == N)
+            return n;
+    }
+}
 int main()
 {
     int choose;
     int i=0;
+    int n;
+    char A,B;
 
     while(choose != 4)
     {
@@ -51,33 +67,59 @@ int main()
 
         switch(choose)
         {
-        case 1:
+        case 1://加入常數
             i++;
             printf("Enter a Letter : ");
             fflush(stdin);
             scanf("%c", &constant.Letter[i]);
-            if(check(constant.Letter[i],i) == 0)
-                break;
+            if(check_1(constant.Letter[i],i) == 0)
+            {
+                printf("Already have this Constant");
+                i = i-1;
+            }
             else
             {
                 printf("Enter a Constant : ");
                 scanf("%lf", &constant.num[i]);
                 printf("%c = %lf\n", constant.Letter[i], constant.num[i]);
-
-                printf("\n\n");
             }
+            printf("\n\n");
             break;
-        case 2:                  //還沒做
+        case 2://刪除
+            printf("Enter a Letter to be deleted : ");
+            fflush(stdin);
+            scanf("%c", &B);
+
+            if(check_2(B,i) == 0)
+            {
+                constant.Letter[del(B,i)] = '\0';
+                del(B,i);
+                printf("%c has been deleted.\n", B);
+                printf("%d\n", del(B,i));
+                printf("%c\n", constant.Letter[del(B,i)]);
+            }
+            else
+            {
+                printf("Can't found this Constant.\n");
+            }
 
             printf("\n\n");
             break;
-        case 3:
-        	printf("Enter a Letter : ");
-        	fflush(stdin);
-            scanf("%c", &constant.Letter[i]);
-            search(constant.Letter[i],i);
-            
+        case 3://搜尋
+            printf("Enter a Letter to be search : ");
+            fflush(stdin);
+            scanf("%c", &A);
+
+            if(search(A,i) <= i)
+                printf("---%lf\n",constant.num[search(A,i)]);
+            else
+                printf("Can't found this Constant.\n");
             printf("\n\n");
+            break;
+        case 4:
+            break;
+        default:
+            printf( "Invalid choice.\n\n" );
             break;
         }
     }
