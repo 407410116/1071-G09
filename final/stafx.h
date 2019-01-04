4a2475b28900b1f24c9103be7d58f9e1d323ebbc
jim890717 <43335027+jim890717@users.noreply.github.com>
2019-01-04 21:58:32 +0800

    Create stafx.h

---
+++ final/stafx.h
@@ -0,0 +1,10 @@
+#ifndef STAFX_H_INCLUDED
+#define STAFX_H_INCLUDED
+
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 100
+#include<conio.h>
+#include <string.h>
+#include <math.h>
+#endif // STAFX_H_INCLUDED
